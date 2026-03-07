e967a5181fb1935119cd48a199c6323881274f52
Atulchaudhary92 <atul.chaudhary24b@iiitg.ac.in>
2026-03-07 18:17:58 +0000

    object

---
+++ basics/object.js
@@ -0,0 +1,20 @@
+const heros =["shakitman","spiderman"]
+//object 
+let myObj={
+    name:"atul",
+    age : 21,
+}
+
+const id=Symbol("1234")
+const myFunction=function(){
+    
+}
+console.log(typeof id)
+
+//  nums and math
+const balance=new Number(1001);
+// converting into string
+    console.log(balance.toString());
+// fix decimal place
+
+console.log(balance.toFixed(1));
