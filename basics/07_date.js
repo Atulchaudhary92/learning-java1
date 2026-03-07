67ec462920341156c6a8d6d464e99d981d1670b8
Atulchaudhary92 <atul.chaudhary24b@iiitg.ac.in>
2026-03-07 18:42:10 +0000

    date

---
+++ basics/07_date.js
@@ -0,0 +1,20 @@
+// let myDate=new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// create your own date
+// let myCreateDate=new Date(2024,5,24)
+// second format
+// myCreateDate=new Date(2024,3,25,5,4);
+// console.log(myCreateDate.toLocaleString());
+// 4/25/2024, 5:04:00 AM
+// console.log(myCreateDate.toString);
+// Thu Apr 25 2024 05:04:00 GMT+0000 (Coordinated Universal Time)
+
+//  let myCreateDate=new Date("01-15-2024")
+// console.log(myCreateDate.toLocaleString());
+// 1/15/2024, 12:00:00 AM
+
+let myTimeStamp=Date.now()
+console.log(myTimeStamp)
