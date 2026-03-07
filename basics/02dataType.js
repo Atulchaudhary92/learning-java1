b3179188d15aa72a903890f533ec9f3bd17cd163
Atulchaudhary92 <atul.chaudhary24b@iiitg.ac.in>
2026-03-07 08:01:26 +0000

    container config done

---
+++ basics/02dataType.js
@@ -0,0 +1,14 @@
+let name="atul"
+let age =21
+let isLoggedIn=false
+// number=> 2 to power 63-10
+// bigint
+// boolean=> true false
+// null=>standalone value
+// undefined
+// symbol
+
+
+// object 
+ 
+console.log(typeof undefined);
