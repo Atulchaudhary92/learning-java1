64bb18f9083cd0a975dede7740248996d1351a63
Atulchaudhary92 <atul.chaudhary24b@iiitg.ac.in>
2026-03-08 05:24:09 +0000

    array

--- basics/array.js
+++ basics/array.js
@@ -28,13 +28,38 @@ const myArray=new Array(1,2,3,4)
 
 
 // slice ,splice
-console.log("A",myArray);
-const myn1=myArray.slice(1,3);
-console.log("B",myn1);
-const myn2=myArray.splice(1,3);
-console.log("C",myArray);
-console.log("D",myn2);
-A [ 1, 2, 3, 4 ]
-B [ 2, 3 ]
-C [ 1 ]
-D [ 2, 3, 4 ]
+// console.log("A",myArray);
+// const myn1=myArray.slice(1,3);
+// console.log("B",myn1);
+// const myn2=myArray.splice(1,3);
+// console.log("C",myArray);
+// console.log("D",myn2);
+// A [ 1, 2, 3, 4 ]
+// B [ 2, 3 ]
+// C [ 1 ]
+// D [ 2, 3, 4 ]
+
+//  join two arrays
+
+const marvel_heros=["spiderman","ironman"]
+const dc_heros=["superman","flash"]
+// 1.push()
+// 2.concat()
+// 3.spread()
+
+// const all_heros=[...dc_heros,...marvel_heros,...myArray]
+// console.log(all_heros);
+// [ 'superman', 'flash', 'spiderman', 'ironman', 1, 2, 3, 4 ]
+
+// console.log(Array.isArray("Atul"))
+// false
+// console.log(Array.from("Atuul"))
+// [ 'A', 't', 'u', 'u', 'l' ]
+
+// console.log(Array.from({name:"Atul"}))
+// []
+
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1,score2,score3))
