2e535ec605c2807ac98ef1bddbb986fd7c133ccc
Atulchaudhary92 <atul.chaudhary24b@iiitg.ac.in>
2026-03-08 05:24:43 +0000

    array

--- basics/array.js
+++ basics/array.js
@@ -59,7 +59,8 @@ const dc_heros=["superman","flash"]
 // console.log(Array.from({name:"Atul"}))
 // []
 
-let score1=100
-let score2=200
-let score3=300
-console.log(Array.of(score1,score2,score3))
+// let score1=100
+// let score2=200
+// let score3=300
+// console.log(Array.of(score1,score2,score3))
+// [ 'A', 't', 'u', 'u', 'l' ]
