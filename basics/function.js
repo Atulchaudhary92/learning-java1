47cd9da951d011627417436b127f8f4d84d4efd1
Atulchaudhary92 <atul.chaudhary24b@iiitg.ac.in>
2026-03-11 13:54:34 +0000

    function

---
+++ basics/function.js
@@ -0,0 +1,30 @@
+// function add(a,b){
+//     return a+b;
+// }
+// function execute(a,b,functionName){
+//     return functionName(a,b);
+// }
+
+// // console.log(execute(1,4,add));
+
+// handleObject({
+//     userName:"Atul",
+//     age:22
+// })
+
+// const myNewArray=[200,400,100,600]
+// function returnSEconValue(getArray){
+//     reuturn getArray
+// }
+
+
+// this function 
+// /
+// console.log(addOne(3))
+// 4
+
+// implicit return for one line statement that does not need to return the value.
+const addone=(num1)=>( num1+1) 
+// returning an object
+const object=()=>
+console.log(addone(3))
